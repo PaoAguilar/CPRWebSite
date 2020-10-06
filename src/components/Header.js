9fae3cd6e477d04f852a478b7762ddbe62dc962e
Paola Aguilar <pao.aquev29@gmail.com>
2020-10-06 14:07:31 -0600

    arreglando detalles en acerca de y trabajos

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,11 +11,7 @@ const Header = props => (
       <div className="inner">
         <h1>Quevedo Consultores</h1>
         <p>
-          A fully responsive site template designed by{' '}
-          <a href="https://html5up.net">HTML5 UP</a> and released
-          <br />
-          for free under the{' '}
-          <a href="https://html5up.net/license">Creative Commons</a> license.
+          Hagámoslo realidad con Analisis Predictivo
         </p>
       </div>
     </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,8 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import pic01 from '../images/pic01.jpg'
-import pic02 from '../images/pic02.jpg'
-import pic03 from '../images/pic03.jpg'
+import pred1 from '../images/pred1.jpg'
+import pred2 from '../images/pred2.jpg'
+import pred3 from '../images/pred3.jpg'
 
 class Main extends React.Component {
   render() {
@@ -25,61 +25,22 @@ class Main extends React.Component {
           id="intro"
           className={`${this.props.article === 'intro' ? 'active' : ''} ${
             this.props.articleTimeout ? 'timeout' : ''
-          }`}
+            }`}
           style={{ display: 'none' }}
         >
           <h2 className="major">Intro</h2>
           <span className="image main">
-            <img src={pic01} alt="" />
+            <img src={pred1} alt="" />
           </span>
           <p>
-            Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin
-            aliquam facilisis ante interdum congue. Integer mollis, nisl amet
-            convallis, porttitor magna ullamcorper, amet egestas mauris. Ut
-            magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas.
-            By the way, check out my <a href="#work">awesome work</a>.
+            <h5>Misión</h5>
+            Brindar soluciones por medio de análisis predictivo y utilizando las mejores tecnologías del mercado,
+            para que nuestros clientes puedan tener información valiosa para tomar decisiones.
           </p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-            dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora
-            torquent per conubia nostra, per inceptos himenaeos. Etiam tristique
-            libero eu nibh porttitor fermentum. Nullam venenatis erat id
-            vehicula viverra. Nunc ultrices eros ut ultricies condimentum.
-            Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae
-            dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in
-            lectus. Pellentesque habitant morbi tristique senectus et netus et
-            malesuada fames ac turpis egestas. In non lorem sit amet elit
-            placerat maximus. Pellentesque aliquam maximus risus, vel sed
-            vehicula.
-          </p>
-          {close}
-        </article>
-
-        <article
-          id="work"
-          className={`${this.props.article === 'work' ? 'active' : ''} ${
-            this.props.articleTimeout ? 'timeout' : ''
-          }`}
-          style={{ display: 'none' }}
-        >
-          <h2 className="major">Work</h2>
-          <span className="image main">
-            <img src={pic02} alt="" />
-          </span>
-          <p>
-            Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu,
-            at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent
-            urna nisi, fringila lorem et vehicula lacinia quam. Integer
-            sollicitudin mauris nec lorem luctus ultrices.
-          </p>
-          <p>
-            Nullam et orci eu lorem consequat tincidunt vivamus et sagittis
-            libero. Mauris aliquet magna magna sed nunc rhoncus pharetra.
-            Pellentesque condimentum sem. In efficitur ligula tate urna.
-            Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor.
-            Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis
-            libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat
-            tempus.
+            <h5>Visión</h5>
+            Ser la empresa consultora líder de análisis predictivo en Centro América,
+            apoyando a nuestros clientes por medio de información valiosa acerca de riesgos y oportunidades futuras.
           </p>
           {close}
         </article>
@@ -88,21 +49,50 @@ class Main extends React.Component {
           id="about"
           className={`${this.props.article === 'about' ? 'active' : ''} ${
             this.props.articleTimeout ? 'timeout' : ''
-          }`}
+            }`}
           style={{ display: 'none' }}
         >
-          <h2 className="major">About</h2>
+          <h2 className="major">Acerca</h2>
           <span className="image main">
-            <img src={pic03} alt="" />
+            <img src={pred2} alt="" />
           </span>
           <p>
-            Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent
-            eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam
-            erat volutpat. Praesent urna nisi, fringila lorem et vehicula
-            lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.
-            Aliquam libero et malesuada fames ac ante ipsum primis in faucibus.
-            Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit
-            amet.
+            Quevedo Consultores S.A. de C.V. presenta a sus clientes la oportunidad de optar por consultoría sobre
+            la implantación de soluciones que empleen el análisis predictivo para anticiparse a los resultados de los
+            productos o servicios que tengan en el mercado, identificar nuevas oportunidades de crecimiento,
+            así como tomar medidas para optimizar y hacer más eficiente el negocio basándose en el mencionado análisis,
+            entre otros beneficios. Esto a través del empleo de conocimientos, técnicas y herramientas basadas en BigData.
+          </p>
+          {close}
+        </article>
+
+        <article
+          id="work"
+          className={`${this.props.article === 'work' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+            }`}
+          style={{ display: 'none' }}
+        >
+          <h2 className="major">Trabajos</h2>
+          <span className="image main">
+            <img src={pred3} alt="" />
+          </span>
+          <p>
+            <h5>Análisis predictivo</h5>
+            Realizamos un plan de marketing digital más efectivo ya que previamente se analizan
+            los datos para averiguar el comportamiento de un cliente y así poder basar el tipo de estrategia
+            a utilizar.
+          </p>
+          <p>
+            <h5>Retención de clientes</h5>
+            Ya que retener a los clientes que ya se tienen es menos costoso que adquirir nuevos,
+            el análisis predictivo puede proporcionar una visión profunda del comportamiento de la
+            rotación de clientes en la empresa.
+          </p>
+          <p>
+            <h5>Optimización web</h5>
+            Se puede profundizar en la forma en que los visitantes interactúan y se involucran en la
+            web mediante el procesamiento de docenas de variables de datos a través de modelos predictivos.
           </p>
           {close}
         </article>
@@ -111,13 +101,13 @@ class Main extends React.Component {
           id="contact"
           className={`${this.props.article === 'contact' ? 'active' : ''} ${
             this.props.articleTimeout ? 'timeout' : ''
-          }`}
+            }`}
           style={{ display: 'none' }}
         >
-          <h2 className="major">Contact</h2>
+          <h2 className="major">Contacto</h2>
           <form method="post" action="#">
             <div className="field half first">
-              <label htmlFor="name">Name</label>
+              <label htmlFor="name">Nombre</label>
               <input type="text" name="name" id="name" />
             </div>
             <div className="field half">
@@ -125,7 +115,7 @@ class Main extends React.Component {
               <input type="text" name="email" id="email" />
             </div>
             <div className="field">
-              <label htmlFor="message">Message</label>
+              <label htmlFor="message">Mensaje</label>
               <textarea name="message" id="message" rows="4"></textarea>
             </div>
             <ul className="actions">
@@ -140,25 +130,25 @@ class Main extends React.Component {
           <ul className="icons">
             <li>
               <a
-                href="https://twitter.com/HuntaroSan"
+                href=""
                 className="icon fa-twitter"
               >
                 <span className="label">Twitter</span>
               </a>
             </li>
             <li>
-              <a href="https://codebushi.com" className="icon fa-facebook">
+              <a href="" className="icon fa-facebook">
                 <span className="label">Facebook</span>
               </a>
             </li>
             <li>
-              <a href="https://codebushi.com" className="icon fa-instagram">
+              <a href="" className="icon fa-instagram">
                 <span className="label">Instagram</span>
               </a>
             </li>
             <li>
               <a
-                href="https://github.com/codebushi/gatsby-starter-dimension"
+                href=""
                 className="icon fa-github"
               >
                 <span className="label">GitHub</span>
@@ -167,7 +157,7 @@ class Main extends React.Component {
           </ul>
           {close}
         </article>
-      </div>
+      </div >
     )
   }
 }
