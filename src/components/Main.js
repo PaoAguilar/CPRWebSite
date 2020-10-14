f3c59ab7f3c962db8c4bcd2f24769d89ca340b6b
Paola Aguilar <pao.aquev29@gmail.com>
2020-10-14 10:37:39 -0600

    agregando redes sociales

--- src/components/Main.js
+++ src/components/Main.js
@@ -130,28 +130,20 @@ class Main extends React.Component {
           <ul className="icons">
             <li>
               <a
-                href=""
+                href="https://twitter.com/QuevedoConsult1" target="_blank"
                 className="icon fa-twitter"
               >
                 <span className="label">Twitter</span>
               </a>
             </li>
             <li>
-              <a href="" className="icon fa-facebook">
+              <a href="https://www.facebook.com/Quevedo-consultores-107186371165389/" target="_blank" className="icon fa-facebook">
                 <span className="label">Facebook</span>
               </a>
             </li>
             <li>
-              <a href="" className="icon fa-instagram">
+              <a href="https://www.instagram.com/quevedoconsultores/" target="_blank" className="icon fa-instagram">
                 <span className="label">Instagram</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href=""
-                className="icon fa-github"
-              >
-                <span className="label">GitHub</span>
               </a>
             </li>
           </ul>
